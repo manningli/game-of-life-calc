58414df6b2d928e30c8f7b45d679f9b6418df0c6
Lawrence M <39105751+manningli@users.noreply.github.com>
2023-11-25 19:07:07 -0500

    v2 start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { LifeTilesComponent } from './pages/life-tiles/life-tiles.component';
-import { MoneyComponent } from './pages/money/money.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { PlayerComponent } from './pages/player/player.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    title: 'Game of Life Calculator | Home',
-    component: HomeComponent,
-  },
-  {
-    path: 'player',
-    title: 'Game of Life Calculator | Player Info',
-    component: PlayerComponent,
-  },
-  {
-    path: 'money',
-    title: 'Game of Life Calculator | Money',
-    component: MoneyComponent,
-  },
-  {
-    path: 'life-tiles',
-    title: 'Game of Life Calculator | Life Tiles',
-    component: LifeTilesComponent,
-  },
-  {
-    path: '**',
-    component: PageNotFoundComponent,
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,46 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { HomeComponent } from './pages/home/home.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
-import { PlayerComponent } from './pages/player/player.component';
-import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MoneyComponent } from './pages/money/money.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { LifeTilesComponent } from './pages/life-tiles/life-tiles.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    PlayerComponent,
-    MoneyComponent,
-    PageNotFoundComponent,
-    LifeTilesComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatInputModule,
-    FormsModule,
-    ReactiveFormsModule,
+    AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
