3845fd9de2b4a470f6a4f7a27fcd5d2bff82ea81
Lawrence M. <39105751+manningli@users.noreply.github.com>
2024-01-13 22:17:14 -0500

    reroute to home from calc if currentPlayer is not set

--- src/app/pages/calculator/calculator.component.ts
+++ src/app/pages/calculator/calculator.component.ts
@@ -1,8 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss'],
 })
-export class CalculatorComponent {}
+export class CalculatorComponent {
+  constructor(private router: Router) {
+    if (!sessionStorage.getItem('currentPlayer')) {
+      this.router.navigateByUrl('');
+    }
+  }
+}
